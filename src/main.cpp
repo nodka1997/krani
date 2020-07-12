57c79ac083340605951dc1d05b40c21158e6b784
nodo <asdf@ass.com>
2020-07-12 14:17:13 +0400

    asdasdasda

---
+++ src/main.cpp
@@ -0,0 +1,52 @@
+#include <Arduino.h>
+
+const int motorCCW = 5;
+const int motorCW = 4;
+const int valveClose = 12;
+const int valveOpen = 14;
+const int btnValveOpen = 13;
+const int btnValveClose = 15;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(motorCCW, OUTPUT);
+  pinMode(motorCW, OUTPUT);
+  pinMode(valveClose, INPUT);
+  pinMode(valveOpen, INPUT);
+  pinMode(btnValveOpen, INPUT);
+  pinMode(btnValveClose, INPUT);
+}
+
+void loop()
+{
+
+  if (digitalRead(btnValveOpen) == LOW)
+  {
+    digitalWrite(motorCCW, HIGH);
+  }
+  else
+  {
+    digitalWrite(motorCCW, LOW);
+  }
+
+  if (digitalRead(btnValveClose) == LOW)
+  {
+    digitalWrite(motorCW, HIGH);
+  }
+  else
+  {
+    digitalWrite(motorCW, LOW);
+  }
+
+  if (digitalRead(valveOpen) == digitalRead(btnValveOpen))
+  {
+    digitalWrite(motorCCW, LOW);
+  }
+
+  if (digitalRead(valveClose) == digitalRead(btnValveClose))
+  {
+    digitalWrite(motorCW, LOW);
+  }
+
+}
