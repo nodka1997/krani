15593f76388251fb119cd649db48f535181ce942
nodo <asdf@ass.com>
2020-07-12 15:06:50 +0400

    going to test old code

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,16 @@
 
 const int motorCCW = 5;
 const int motorCW = 4;
-const int valveClose = 12;
-const int valveOpen = 14;
+const int valveClose = 14;
+const int valveOpen = 12;
 const int btnValveOpen = 13;
 const int btnValveClose = 15;
 
+void resetState();
+
 void setup()
 {
+
   Serial.begin(9600);
   pinMode(motorCCW, OUTPUT);
   pinMode(motorCW, OUTPUT);
@@ -17,9 +20,24 @@ void setup()
   pinMode(btnValveOpen, INPUT);
   pinMode(btnValveClose, INPUT);
 
+  Serial.println("above resetState()");
+  resetState();
+  Serial.println("below resetState()");
 }
 
 void loop()
 {
 
+}
+
+void resetState()
+{
+  Serial.println(digitalRead(valveClose));
+  while (digitalRead(valveClose) == 1)
+  {
+    Serial.println("looping");
+    digitalWrite(btnValveClose, 1);
+  }
+
+  digitalWrite(btnValveClose, 0);
 }
